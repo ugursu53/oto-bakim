708ef729cc7cfeb3dac8e3942de040ae818b713d
ugursu53 <ugursu53@gmail.com>
2019-08-17 13:12:41 +0300

    görsel düzeltmeler

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -145,8 +145,8 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
             utils.root('src/test'),
         ]),
         new WebpackNotifierPlugin({
-            title: 'JHipster',
-            contentImage: path.join(__dirname, 'logo-jhipster.png')
+            title: 'OtoBakim',
+            contentImage: path.join(__dirname, 'logo-mavi.jpg')
         })
     ].filter(Boolean),
     mode: 'development'
